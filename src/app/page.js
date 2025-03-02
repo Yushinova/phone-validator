abd0af363b0dbfc515caba0953254b4185fd04f9
Yushi_np28@top-academy.ru <Yushi_np28@top-academy.ru>
2025-03-02 12:36:35 +0300

    add validator

--- src/app/page.js
+++ src/app/page.js
@@ -1,95 +1,67 @@
+"use client"
 import Image from "next/image";
 import styles from "./page.module.css";
+import { useRef, useState, useEffect } from "react";
+import 'bootstrap/dist/css/bootstrap.css';
 
+function PhoneValidator(props) {
+  let nRef = useRef(null);
+  //чтобы при начале работы не срабатывала валидация
+  let isStart=true;
+  // инициалиазция state-ов
+  let [result, setResult] = useState("");
+  
+//функция проврки телефона
+function validatePhoneNumber() {
+  const regex = /\+\d\(\d{3}\)\d{3}-\d{2}-\d{2}$/;
+ let isValid = regex.test(nRef.current.value);
+  if(!isValid){
+    setResult("Не верный формат ввода! Пример ( +X(XXX)XXX-XX-XX )")
+    return
+  }
+  setResult("Ввод верный")
+  console.log(isValid)
+}
+// инициализация изначального значения input
+useEffect(() => {
+  // на момент срабатывания данного колбэка 
+  // уже сработал return и nRef привязался к input-у
+  if(isStart){
+    nRef.current.value=""
+  }
+  else{
+    validatePhoneNumber();
+    //сбрасываем флаг
+    isStart=false
+  }
+}, []);
+
+ // обработчик отправки формы
+ function onFormSubmit(event) {
+  event.preventDefault();
+  validatePhoneNumber();
+
+}
+  return(
+    <div className="container-sm">
+        {/* форма для вычисления факториала */}
+        <form onSubmit={onFormSubmit}>
+            {/* поле ввода n */}
+            <label htmlFor="phone" className="form-label">Введите номер телефона:</label>
+            <input type="tel" id="phone" required ref={nRef} className="form-control" placeholder="+X(XXX)XXX-XX-XX"/>
+            {/* кнопка отправки формы */}
+            <button type="submit" className="btn btn-primary"> Проверить</button>
+          </form>
+          {/* вывод результата */}
+          <p>{result}</p>
+    </div>
+  );
+  
+}
 export default function Home() {
   return (
     <div className={styles.page}>
-      <main className={styles.main}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol>
-          <li>
-            Get started by editing <code>src/app/page.js</code>.
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
-
-        <div className={styles.ctas}>
-          <a
-            className={styles.primary}
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className={styles.logo}
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-            className={styles.secondary}
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className={styles.footer}>
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+     <PhoneValidator/>
     </div>
   );
 }
